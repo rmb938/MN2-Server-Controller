d34fdd7158c1ccc5e2395c06c43ca03308b4fb0f
Jodie Belgrave <rmb1993@gmail.com>
2014-04-02 15:57:02 -0400

    Added log4j2 and jline

---
+++ src/main/java/com/rmb938/controller/MN2ServerController.java
@@ -0,0 +1,16 @@
+package com.rmb938.controller;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class MN2ServerController {
+
+    private static final Logger logger = LogManager.getLogger(MN2ServerController.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Starting Server Controller");
+    }
+
+}
