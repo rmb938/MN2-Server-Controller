0a598033e8d272e8f988a8278e2db1293fd8133b
Jodie Belgrave <rmb1993@gmail.com>
2014-04-04 12:23:43 -0400

    yay starting new servers :D

--- src/main/java/com/rmb938/controller/threads/ServerManager.java
+++ src/main/java/com/rmb938/controller/threads/ServerManager.java
@@ -4,10 +4,16 @@ import com.rmb938.controller.MN2ServerController;
 import com.rmb938.controller.entity.RemoteController;
 import com.rmb938.controller.entity.Server;
 import com.rmb938.controller.entity.ServerInfo;
+import com.rmb938.jedis.JedisManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import redis.clients.jedis.Jedis;
 
 import java.util.ArrayList;
 
 public class ServerManager implements Runnable {
+
+    private static final Logger logger = LogManager.getLogger(ServerManager.class.getName());
 
     private final MN2ServerController serverController;
 
@@ -29,14 +35,55 @@ public class ServerManager implements Runnable {
                 Server.getServers().remove(serverName);
             }
 
+            for (ServerInfo serverInfo : ServerInfo.getServerInfos().values()) {
+                Jedis jedis = JedisManager.getJedis();
+                if (jedis.exists(serverInfo.getServerName()) == false) {
+                    continue;
+                }
+                if (jedis.setnx("lock."+serverInfo.getServerName(), System.currentTimeMillis()+30000+"") == 0) {
+                    long time = Long.parseLong(jedis.get("lock." + serverInfo.getServerName()));
+                    if (System.currentTimeMillis() > time) {
+                        time = Long.parseLong(jedis.getSet("lock."+serverInfo.getServerName(), System.currentTimeMillis()+30000+""));
+                        if (System.currentTimeMillis() < time) {
+                            JedisManager.returnJedis(jedis);
+                            return;
+                        }
+                    } else {
+                        JedisManager.returnJedis(jedis);
+                        return;
+                    }
+                }
+                int size = Integer.parseInt(jedis.get(serverInfo.getServerName()));
+                //TODO: check if can have more servers
+                //TODO: find port for new server
+                int port = -1;
+                Server server = new Server(serverController, serverInfo, port);
+                server.startServer();
+                int finalSize = size -1;
+                if (finalSize == 0) {
+                    jedis.del(serverInfo.getServerName());
+                } else {
+                    jedis.set(serverInfo.getServerName(), finalSize+"");
+                }
+                jedis.del("lock."+serverInfo.getServerName());
+                JedisManager.returnJedis(jedis);
+            }
+
             //Start up more servers if less then min needed
             if (RemoteController.getMainController() != null) {
                 if (RemoteController.getMainController().getIP().equalsIgnoreCase(serverController.getControllerIP())) {
                     for (ServerInfo serverInfo : ServerInfo.getServerInfos().values()) {
-                        int size = Server.getServers(serverInfo).size();
-                        if (serverInfo.getMinServers() < size) {
-
+                        Jedis jedis = JedisManager.getJedis();
+                        if (jedis.exists(serverInfo.getServerName())) {
+                            int size = Integer.parseInt(jedis.get(serverInfo.getServerName()));
+                            logger.warn("Waiting to create "+size+" servers of "+serverInfo.getServerName()+" if this continues please check server status.");
+                            continue;
                         }
+                        int size = Server.getServers(serverInfo).size();
+                        if (serverInfo.getMinServers() > size) {
+                            jedis.set(serverInfo.getServerName(), serverInfo.getMinServers() - size+"");
+                        }
+                        JedisManager.returnJedis(jedis);
                     }
                 }
             }
